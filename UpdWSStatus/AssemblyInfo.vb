cbc396f47fbaaaf89772c9868ab79b997ff823bd
Emanuele Gualtierotti <WIN-DP9DE1R33JN\EGualtierotti>
2016-11-07 16:56:22 +0000

    Aggiunto stato FUNCTION_IS_RUNNING (999) per segnalare che una funzione POS è in esecuzione. E' necessario modificare opportunamente la tabella BatchActivity per rendere effettiva la modifica.
    git-tfs-id: [http://88.37.239.142:8080/tfs]$/TPNetTest/TPCusDev/IT/Common/dev.net/Pos/Tools;C291

---
+++ UpdWSStatus/AssemblyInfo.vb
@@ -0,0 +1,33 @@
+Imports System.Reflection
+Imports System.Runtime.CompilerServices
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly
+
+
+' TODO: Review the values of the assembly attributes
+
+
+<Assembly: AssemblyTitle("Update Workstation Status")> 
+<Assembly: AssemblyDescription("Update Workstation Status")> 
+<Assembly: AssemblyCompany("Wincor Nixdorf")>
+<Assembly: AssemblyProduct("TP.net")>
+<Assembly: AssemblyCopyright("Copyright (c) 1997 - 2013 Wincor Nixdorf International GmbH")>
+<Assembly: AssemblyTrademark("TP.net")>
+<Assembly: AssemblyCulture("")>
+
+' Version information for an assembly consists of the following four values:
+
+'	Major version
+'	Minor Version
+'	Revision
+'	Build Number
+
+' You can specify all the values or you can default the Revision and Build Numbers
+' by using the '*' as shown below
+
+
+<Assembly: AssemblyVersion("50.17.02.00")>
+<Assembly: AssemblyFileVersion("50.17.02.00")>
