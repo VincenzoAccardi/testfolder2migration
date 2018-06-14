336d875ea2cb27128a36245ab0e8e2286e8e03dc
Vincenzo Accardi <WIN-DP9DE1R33JN\VAccardi>
2018-06-14 08:50:25 +0000

    Aggiunta gestione richiesta. Sono stati aggiunti dei parametri per posticipare la stampa. Adesso la stampa (su scontrino separato o su quello di vendita) viene demandata ad un metodo di conferma. Per il corretto funzionamento i parametri EXT_GC_ACTIVATION_PRINT_WITHIN_TA e EXT_GC_DEACTIVATION_PRINT_WITHIN_TA devono essere impostati a Y per permettere al metodo di conferma di recuperare i dati delle ricevute. Impostare anche i campi EXT_GC_ACTIVATION_COPIES e EXT_GC_DEACTIVATION_COPIES a 0 in maniera tale da non stampare le ricevute in concomitanza della vendita o l'annullo dell'articolo. I parametri per la conferma sono i seguenti: EXT_GC_CONFIRM_COPIES : numero di copie da stampare EXT_GC_CONFIRM_PRINT_WITHIN_TA : visualizzare la ricevuta all'interno dello scontrino
    git-tfs-id: [http://88.37.239.142:8080/tfs]$/TPNetTest/TPCusDev/IT/Common/dev.net/Pos/Interfaces;C933

---
+++ IExternalGiftCard/IExternalGiftCardConfirm.vb
@@ -0,0 +1,4 @@
+﻿Public Interface IExternalGiftCardConfirm
+    Function ConfirmExternalGiftCard(ByRef Parameters As System.Collections.Generic.Dictionary(Of String, Object)) As IExternalGiftCardReturnCode
+
+End Interface
