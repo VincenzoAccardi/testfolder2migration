47f990ab510f833c7cf42a99f9644938cf6448e7
Carmel Daniele <WIN-DP9DE1R33JN\CDaniele>
2018-09-26 07:41:37 +0000

    Nuovi elementi
    git-tfs-id: [http://88.37.239.142:8080/tfs]$/TPNetTest/TPCusDev/IT/Common/dev.net/Pos/Interfaces;C982

---
+++ IBuoniPasto/IBPCDematerialize.vb
@@ -0,0 +1,17 @@
+﻿#Region "Documentation"
+' ********** ********** ********** **********
+' IBPCDematerialize
+' ---------- ---------- ---------- ----------
+' Author : Emanuele Gualtierotti
+' Wincor Nixdorf Retail Consulting
+' -----------------------------------
+' Copyright by Wincor Nixdorf Retail Consulting
+' 20090, Basiglio, 2014, All rights reserved.
+' -----------------------------------
+#End Region
+
+#Region "IBPCDematerialize"
+Public Interface IBPCDematerialize
+    Function Dematerialize(ByRef Parameters As System.Collections.Generic.Dictionary(Of String, Object)) As IBPReturnCode
+End Interface
+#End Region
---
+++ IBuoniPasto/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("NF_IT_IBuoniPasto")>
+<Assembly: AssemblyDescription("Gestione Buoni Pasto Cartaceo ed Elettronico tramite Argentea")>
+<Assembly: AssemblyCompany("OEM")> 
+<Assembly: AssemblyProduct("NF_IT_IBuoniPasto")> 
+<Assembly: AssemblyCopyright("Copyright © OEM 2018")>
+<Assembly: AssemblyTrademark("RETEX")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("c7dbcb39-4511-44b5-8eb3-03c168fa6bfd")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("50.17.02.00")>
+<Assembly: AssemblyFileVersion("50.17.02.00")>
