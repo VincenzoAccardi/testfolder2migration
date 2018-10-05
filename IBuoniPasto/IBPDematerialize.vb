7b02c101efe3491c8d2309d49d1f64fad185b664
Carmel Daniele <WIN-DP9DE1R33JN\CDaniele>
2018-10-05 10:53:23 +0000

    Storno
    git-tfs-id: [http://88.37.239.142:8080/tfs]$/TPNetTest/TPCusDev/IT/Common/dev.net/Pos/Interfaces;C984

--- IBuoniPasto/IBPDematerialize.vb
+++ IBuoniPasto/IBPDematerialize.vb
@@ -16,6 +16,31 @@ Imports TPDotnet.IT.Common.Pos
 
 #Region "IBPDematerialize"
 Public Interface IBPDematerialize
+
+    ''' <summary>
+    '''     Azione di Dematerializzazione dei BP sia
+    '''     Elettronici (Richiamerà il proxy in modalità Hardware POS)
+    '''     Cartacei (Richiamerà il roxy in modalità Software SERVICE)
+    ''' </summary>
+    ''' <param name="Parameters">
+    '''     Il Set di parametri dinamici ad uso e consumo
+    '''     del Controller che implementa il metodo passati
+    '''     in modo dinamico previsti sul DB di BackStore
+    ''' </param>
+    ''' <returns></returns>
     Function Dematerialize(ByRef Parameters As System.Collections.Generic.Dictionary(Of String, Object)) As IBPReturnCode
+
+    ''' <summary>
+    '''     Azione di Storno
+    '''     (direttamente da voce di selezione in TA principale)
+    ''' </summary>
+    ''' <param name="Parameters">
+    '''     Il Set di parametri dinamici ad uso e consumo
+    '''     del Controller che implementa il metodo passati
+    '''     in modo dinamico previsti sul DB di BackStore
+    ''' </param>
+    ''' <returns></returns>
+    Function Void(ByRef Parameters As System.Collections.Generic.Dictionary(Of String, Object)) As IBPReturnCode
+
 End Interface
 #End Region
