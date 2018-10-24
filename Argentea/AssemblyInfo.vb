0618aa57d1a5f030373367392e6a2196a9aa9a42
Carmel Daniele <WIN-DP9DE1R33JN\CDaniele>
2018-10-24 07:58:22 +0000

    Change Version Assembly
    git-tfs-id: [http://88.37.239.142:8080/tfs]$/TPNetTest/TPCusDev/IT/Common/dev.net/Pos/EFT;C996

--- Argentea/AssemblyInfo.vb
+++ Argentea/AssemblyInfo.vb
@@ -28,7 +28,7 @@ Imports System.Runtime.InteropServices
 ' You can specify all the values or you can default the Revision and Build Numbers
 ' by using the '*' as shown below
 
-<Assembly: AssemblyVersion("50.17.02.43")>
+<Assembly: AssemblyVersion("50.17.02.44")>
 
 
-<Assembly: AssemblyFileVersion("50.17.02.43")>
+<Assembly: AssemblyFileVersion("50.17.02.44")>
