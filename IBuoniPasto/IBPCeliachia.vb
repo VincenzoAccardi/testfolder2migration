160a2c537d3a383ce5de94a8536083cf35827554
Vincenzo Accardi <WIN-DP9DE1R33JN\VAccardi>
2019-01-30 13:59:54 +0000

    Implementata gestione dei buoni celiachia. Vengono presi in considerazione solamente gli articoli che hanno popolato il campo szITSpecialItemType = CELIAC.
    Creare un metodo di pagamento con tipologia pagamento Voucher 2 e szExternalID = BPCeliac 
    
    Aggiungere nella PosModel due record:
    Un record con szModulName=BPCeliac, lModulNmbrExt=7 e szRuntimeDll='TPDotnet.IT.Common.Pos.EFT.Argentea.clsMedia' (prendere spunto dal record con szModulName=clsMedia)
    Un record con szModulName=BPCeliacController, lModulNmbrExt=0 e szRuntimeDll='TPDotnet.IT.Common.Pos.EFT.Argentea.BPCeliacController' (prendere spunto dal record GiftCardController)
    
    Aggiungere nella Parameter due record:
    un record con szObject= Argentea e szKey= BPCELIAC_COPIES con szContents uguale al numero di copie da stampare 
    un record con szObject= Argentea e szKey= BPCELIAC_PRINT_WITHIN_TA e szContents = Y o N se non si vuole stampare la ricevuta nello scontrino o meno 
    
    git-tfs-id: [http://88.37.239.142:8080/tfs]$/TPNetTest/TPCusDev/IT/Common/dev.net/Pos/Interfaces;C1100

---
+++ IBuoniPasto/IBPCeliachia.vb
@@ -0,0 +1,4 @@
+﻿Public Interface IBPCeliachia
+    Function PaymentCeliachia(ByRef Parameters As System.Collections.Generic.Dictionary(Of String, Object)) As IBPReturnCode
+    Function StornoCeliachia(ByRef Parameters As System.Collections.Generic.Dictionary(Of String, Object)) As IBPReturnCode
+End Interface
