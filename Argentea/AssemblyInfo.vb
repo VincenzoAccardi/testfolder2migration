14e182abb6c9923eeed78799992ff2be01a44734
Delivery <WIN-DP9DE1R33JN\Delivery>
2020-01-30 11:24:13 +0000

    Solution and delivery items
    git-tfs-id: [http://88.37.239.142:8080/tfs]$/TPNetTest/TPCusDev/IT/Common/dev.net/Pos/EFT;C1373

--- Argentea/AssemblyInfo.vb
+++ Argentea/AssemblyInfo.vb
@@ -28,7 +28,7 @@ Imports System.Runtime.InteropServices
 ' You can specify all the values or you can default the Revision and Build Numbers
 ' by using the '*' as shown below
 
-<Assembly: AssemblyVersion("5017.02.39.02")>
+<Assembly: AssemblyVersion("5017.02.40.00")>
 
 
-<Assembly: AssemblyFileVersion("5017.02.39.02")>
+<Assembly: AssemblyFileVersion("5017.02.40.00")>
