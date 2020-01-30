874aa36e7ddfcd86faf516724ed568e0709c6fc9
Delivery <WIN-DP9DE1R33JN\Delivery>
2020-01-30 11:24:13 +0000

    Solution and delivery items
    git-tfs-id: [http://88.37.239.142:8080/tfs]$/TPNetTest/TPCusDev/IT/Common/dev.net/Pos/Tools;C1373

--- UpdWSStatus/AssemblyInfo.vb
+++ UpdWSStatus/AssemblyInfo.vb
@@ -29,5 +29,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below
 
 
-<Assembly: AssemblyVersion("5017.02.39.02")>
-<Assembly: AssemblyFileVersion("5017.02.39.02")>
+<Assembly: AssemblyVersion("5017.02.40.00")>
+<Assembly: AssemblyFileVersion("5017.02.40.00")>
