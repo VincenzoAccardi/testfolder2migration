db72a11879e64837f4e444f8a89e204c7249864d
Vincenzo Accardi <WIN-DP9DE1R33JN\VAccardi>
2020-04-27 15:27:10 +0000

    Implementata nuova gestione coupon Valassis con Argentea
    git-tfs-id: [http://88.37.239.142:8080/tfs]$/TPNetTest/TPCusDev/IT/Common/dev.net/Pos/Interfaces;C1408

---
+++ IValassisCoupon/IValassisCouponNotification.vb
@@ -0,0 +1,4 @@
+﻿Public Interface IValassisCouponNotification
+    Function NotificationValassis(ByRef Parameters As System.Collections.Generic.Dictionary(Of String, Object)) As IValassisCouponReturnCode
+
+End Interface
---
+++ IValassisCoupon/IValassisCouponValidation.vb
@@ -0,0 +1,3 @@
+﻿Public Interface IValassisCouponValidation
+    Function ValidationValassis(ByRef Parameters As System.Collections.Generic.Dictionary(Of String, Object)) As IValassisCouponReturnCode
+End Interface
