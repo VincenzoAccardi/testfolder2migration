7a152e4bcd91a4e3757a9ba86f3b3d714d49eea6
Vincenzo Accardi <WIN-DP9DE1R33JN\VAccardi>
2020-11-16 13:43:06 +0000

    Modifiche per implementazione lotteria scontrini
    git-tfs-id: [http://88.37.239.142:8080/tfs]$/TPNetTest/TPCusDev/IT/Common/dev.net/Pos/DataObjects;C1507

---
+++ TaPrinterRT.vb
@@ -0,0 +1,170 @@
+﻿Imports TPDotnet.Pos
+Imports Microsoft.VisualBasic
+Imports System
+Public Class TaPrinterRT
+    Inherits TPDotnet.Pos.TaBaseRec
+
+#Region "Properties"
+    ''' <summary>
+    ''' gets the record type (sId)
+    ''' </summary>
+    ''' <value></value>
+    ''' <returns>Italy_PosDef.TARecTypes.iTA_VLLBALANCE</returns>
+    ''' <remarks></remarks>
+    Public Overrides ReadOnly Property sId() As Short
+        Get
+            Return Italy_PosDef.TARecTypes.iTA_PRINTER_RT
+        End Get
+    End Property
+
+    ''' <summary>
+    ''' gets the object name
+    ''' </summary>
+    ''' <value></value>
+    ''' <returns>"VLLBALANCE"</returns>
+    ''' <remarks></remarks>
+    Public Overrides ReadOnly Property szObjectName() As String
+        Get
+            Return "IT_PRINTERRT"
+        End Get
+    End Property
+
+    ''' <summary>
+    ''' gets/sets the header reference
+    ''' </summary>
+    ''' <value></value>
+    ''' <returns></returns>
+    ''' <remarks></remarks>
+    Private Property lHdrRef() As Integer
+        Get
+            Return m.Fields_Value("lHdrRef")
+        End Get
+        Set(ByVal Value As Integer)
+            m.Fields_Value("lHdrRef") = Value
+        End Set
+    End Property
+
+    ''' <summary>
+    ''' gets the header (TA)
+    ''' </summary>
+    ''' <value></value>
+    ''' <returns></returns>
+    ''' <remarks></remarks>
+    Public Overrides ReadOnly Property theHdr() As TaBaseHdr
+        Get
+            theHdr = m_Hdr
+        End Get
+    End Property
+
+    Public Overridable Property szFiscalCodeOrVatNumber() As String
+        Get
+            Return m.Fields_Value("szFiscalCodeOrVatNumber")
+        End Get
+        Set(ByVal value As String)
+            m.Fields_Value("szFiscalCodeOrVatNumber") = value
+        End Set
+
+    End Property
+
+    Public Overridable Property szLotteryCode() As String
+        Get
+            Return m.Fields_Value("szLotteryCode")
+        End Get
+        Set(ByVal value As String)
+            m.Fields_Value("szLotteryCode") = value
+        End Set
+
+    End Property
+
+#End Region
+
+#Region "New/Finalize"
+
+    ''' <summary>
+    ''' Define standard fields for the TaBistroTableRec object
+    ''' </summary>
+    ''' <remarks></remarks>
+    Protected Overrides Sub DefineFields()
+
+        Try
+            LOG_Info(getLocationString("DefineFields"), "starting")
+
+            MyBase.DefineFields()
+
+            ' create lHdrRef
+            m.Append("lHdrRef", DataField.FIELD_TYPES.FIELD_TYPE_INTEGER)
+
+            '' Standard fields
+            '' ---------------
+            m.Append("szFiscalCodeOrVatNumber", DataField.FIELD_TYPES.FIELD_TYPE_STRING)
+            m.Append("szLotteryCode", DataField.FIELD_TYPES.FIELD_TYPE_STRING)
+
+        Catch ex As Exception
+            Try
+                LOG_Error(getLocationString("DefineFields"), ex)
+
+            Catch InnerEx As Exception
+                LOG_ErrorInTry(getLocationString("DefineFields"), InnerEx)
+            End Try
+        Finally
+            LOG_FuncExit(getLocationString("DefineFields"), "")
+        End Try
+
+    End Sub
+
+    ''' <summary>
+    ''' Initialize the fields defined in DefineFields
+    ''' Set default values
+    ''' </summary>
+    ''' <remarks></remarks>
+    Protected Overrides Sub InitFields()
+
+        Try
+            LOG_Info(getLocationString("InitFields"), "starting")
+
+            MyBase.InitFields()
+
+            'Alpha numeric fields are initialized by '' and numeric fields are initialized by 0.
+
+        Catch ex As Exception
+            Try
+                LOG_Error(getLocationString("InitFields"), ex)
+
+            Catch InnerEx As Exception
+                LOG_ErrorInTry(getLocationString("InitFields"), InnerEx)
+            End Try
+        Finally
+            LOG_FuncExit(getLocationString("InitFields"), "")
+        End Try
+
+    End Sub
+#End Region
+
+#Region "Overwritten functionality"
+    ''' <summary>
+    ''' create new instance of this class
+    ''' </summary>
+    ''' <returns>the new instance of this class</returns>
+    ''' <remarks></remarks>
+    Public Overrides Function CreateMe() As TPDotnet.Pos.TaBaseRec
+        ' create new instance of this class
+        Return New TaServerRT
+    End Function
+
+
+#End Region
+
+#Region "Private Functions"
+
+    ''' <summary>
+    ''' Gets the name of this object and appends it with the actMethod.
+    ''' </summary>
+    ''' <param name="actMethode">The actual method as String</param>
+    ''' <returns>TypeName + method name</returns>
+    ''' <remarks></remarks>
+    Protected Overridable Function getLocationString(ByRef actMethode As String) As String
+        getLocationString = TypeName(Me) & "." & actMethode & " "
+    End Function
+
+#End Region
+End Class
