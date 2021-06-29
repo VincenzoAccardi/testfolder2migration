5cb696f924ae06b5f50b05fcc87dc67c68d9fd6e
Emanuele Gualtierotti <WIN-DP9DE1R33JN\EGualtierotti>
2021-06-29 14:29:48 +0000

    Implementata gestione richiesta
    git-tfs-id: [http://88.37.239.142:8080/tfs]$/TPNetTest/TPCusDev/IT/Common/dev.net/Pos/Interfaces;C1678

---
+++ IKeepAlive/IKeepAlive.vb
@@ -0,0 +1,16 @@
+﻿#Region "Documentation"
+' ********** ********** ********** **********
+' IKeepAlive
+' ---------- ---------- ---------- ----------
+' Author : Emanuele Gualtierotti
+' Wincor Nixdorf Retail Consulting
+' -----------------------------------
+' Copyright by Wincor Nixdorf Retail Consulting
+' 20090, Basiglio, 2014, All rights reserved.
+' -----------------------------------
+#End Region
+#Region "IKeepAlive"
+Public Interface IKeepAlive
+    Sub Run(ByRef TheModCntr As TPDotnet.Pos.ModCntr, ByRef taobj As TPDotnet.Pos.TA)
+End Interface
+#End Region
